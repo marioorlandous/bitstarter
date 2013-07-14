d552fe13972bb6b185aada0f102b7d861b61a3c3
Mario Orlando <mario.orlando.it@gmail.com>
2013-07-14 22:05:17 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,20 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  
+
+  var filename=/home/ubuntu/bitstarter/index.html;
+  var file_content=fs.readFileSync(filename);
+  console.out(filename);
+  console.out(file_content);
+
+
+  response.send(file_content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
