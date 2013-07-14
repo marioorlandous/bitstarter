a84a8849d699ec6814969370ab277e3da4cdac25
Mario Orlando <mario.orlando.it@gmail.com>
2013-07-14 23:11:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,21 +1,21 @@
-var express = require('express');
+#!/usr/bin/env node
 
-var fs=require('fs');
+var fs = require("fs");
 
-var app = express.createServer(express.logger());
+var express = require("express");
 
-var buff = new Buffer(5);buff = fs.readFileSync('index.html');var string1 = buff.toString();console.log(string1 + "\n");
+var buffer = new Buffer(128);
+// buffer = fs.readFileSync("./index.html");
 
+   var app = express.createServer(express.logger());
+// var app = express();
+
+// console.log("Buffer is: " + buffer.toString('utf8'));
 app.get('/', function(request, response) {
-
-  
-  response.send(buff);
-
-
+  response.send(fs.readFileSync("./index.html").toString()) ;
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
